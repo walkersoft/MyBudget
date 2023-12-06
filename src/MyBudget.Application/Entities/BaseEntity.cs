6f0c3f725f769fd92c8936e30de859f28f50d7db
Jason L. Walker <jasonlanewalker@gmail.com>
2023-12-05 21:34:08 -0600

    Created initial expense types

---
+++ src/MyBudget.Application/Entities/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace MyBudget.Application.Entities
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ src/MyBudget.Application/Entities/ExpenseTypes.cs
@@ -0,0 +1,25 @@
+﻿namespace MyBudget.Application.Entities
+{
+    public class BaseExpense : BaseEntity
+    {
+        public string Source { get; set; } = string.Empty;
+        public DateOnly EffectiveDate { get; set; }
+    }
+
+    public class VariableExpense : BaseExpense
+    {
+        public bool IsActive { get; set; }
+    }
+
+    public class StableExpense : BaseExpense
+    {
+        public decimal Amount { get; set; }
+        public bool IsActive { get; set; }
+    }
+
+    public class FixedExpense : BaseExpense
+    {
+        public decimal Amount { get; set; }
+        public DateOnly ExpirationDate { get; set; }
+    }
+}
