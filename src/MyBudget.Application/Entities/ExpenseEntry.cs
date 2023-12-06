c5e227413a9501230770cfb5443819446fb042f9
Jason L. Walker <jasonlanewalker@gmail.com>
2023-12-05 21:51:04 -0600

    Added expense entry entity

---
+++ src/MyBudget.Application/Entities/ExpenseEntry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBudget.Application.Entities
+{
+    public class ExpenseEntry : BaseEntity
+    {
+        public DateOnly CreationDate { get; private set; }
+        public BaseExpense Expense { get; private set; }
+        public decimal Amount { get; private set; }
+        
+        public ExpenseEntry(DateOnly creationDate, BaseExpense expense, decimal amount)
+        {
+            CreationDate = creationDate;
+            Expense = expense;
+            Amount = amount;
+        }
+    }
+}
