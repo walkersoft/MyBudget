c5f586e097ad20df0f4c6672bab80ec95468a5d8
Jason L. Walker <jasonlanewalker@gmail.com>
2023-12-05 21:39:11 -0600

    Added expense category

---
+++ src/MyBudget.Application/Entities/ExpenseCategory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBudget.Application.Entities
+{
+    public class ExpenseCategory : BaseEntity
+    {
+        public string Name { get; set; } = string.Empty;
+        public List<BaseExpense> Expenses { get; private set; } = new();
+    }
+}
