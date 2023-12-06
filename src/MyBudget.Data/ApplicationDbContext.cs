111a05caa50e1ee1e954c95725e388c01a10068c
Jason L. Walker <jasonlanewalker@gmail.com>
2023-12-05 23:04:49 -0600

    Created db context

---
+++ src/MyBudget.Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBudget.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=C:\\temp\\BudgetDB.db");
+        }
+    }
+}
