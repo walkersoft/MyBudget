e0e52df6ba841e06bd4726ad3e8df8a5f48423df
Jason L. Walker <jasonlanewalker@gmail.com>
2023-12-06 19:15:16 -0600

    Added DI and service container init in UI

--- src/MyBudget.UI/App.axaml.cs
+++ src/MyBudget.UI/App.axaml.cs
@@ -1,13 +1,17 @@
-using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Microsoft.Extensions.DependencyInjection;
+using MyBudget.Application;
 using MyBudget.UI.ViewModels;
 using MyBudget.UI.Views;
+using System;
 
 namespace MyBudget.UI;
 
-public partial class App : Application
+public partial class App : Avalonia.Application
 {
+    public static IServiceProvider ServiceProvider { get; private set; }
+
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
@@ -17,6 +21,8 @@ public partial class App : Application
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
+            ConfigureServices();
+
             desktop.MainWindow = new MainWindow
             {
                 DataContext = new MainWindowViewModel(),
@@ -24,5 +30,13 @@ public partial class App : Application
         }
 
         base.OnFrameworkInitializationCompleted();
+    }
+
+    private void ConfigureServices()
+    {
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.ConfigureBudgetApplication();
+
+        ServiceProvider = serviceCollection.BuildServiceProvider();
     }
 }
