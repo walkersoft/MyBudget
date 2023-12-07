ff7fa6cf496f27dc9fc05b603f1c6068144aaab0
Jason L. Walker <jasonlanewalker@gmail.com>
2023-12-06 20:22:07 -0600

    Skeleton for expense creation handler

--- src/MyBudget.Application/Common/BudgetApplication.cs
+++ src/MyBudget.Application/Common/BudgetApplication.cs
@@ -1,6 +1,23 @@
-﻿namespace MyBudget.Application.Common
+﻿using MediatR;
+using MyBudget.Application.Entities;
+using MyBudget.Application.Features.Expenses;
+
+namespace MyBudget.Application.Common
 {
-    public class BudgetApplication(IServiceProvider services)
+    public class BudgetApplication(IMediator mediator)
     {
+
+        public Task<Expense> CreateExpenseAsync()
+        {
+            var request = new CreateExpense(
+                ExpenseType: ExpenseType.Stable,
+                Source: "The void",
+                EffectiveDate: DateOnly.FromDateTime(DateTime.Today),
+                ExpirationDate: DateOnly.FromDateTime(DateTime.Today.AddMonths(6)),
+                Amount: 100m
+            );
+
+            return mediator.Send(request);
+        }
     }
 }
---
+++ src/MyBudget.Application/Features/Expenses/CreateExpense.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using MyBudget.Application.Entities;
+
+namespace MyBudget.Application.Features.Expenses
+{
+    internal record CreateExpense(
+        ExpenseType ExpenseType,
+        string Source,
+        DateOnly EffectiveDate,
+        DateOnly? ExpirationDate,
+        decimal? Amount
+    ) : IRequest<Expense>;
+
+    internal class CreateExpenseHandler() : IRequestHandler<CreateExpense, Expense>
+    {
+        public Task<Expense> Handle(CreateExpense request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
