73ce2b2ba65bc5b6ab64381b40f22a094a569187
Jason L. Walker <jasonlanewalker@gmail.com>
2023-12-06 19:14:13 -0600

    Added MediatR and starter configuration to main application

---
+++ src/MyBudget.Application/ConfigureServices.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace MyBudget.Application
+{
+    public static class ConfigureServices
+    {
+        public static void ConfigureBudgetApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(config => config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        }
+    }
+}
