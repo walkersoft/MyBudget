c838c970bcc49bd3feff1a70b6131c29c5ff9b1b
Jason L. Walker <jasonlanewalker@gmail.com>
2023-12-08 23:38:50 -0600

    Added control to new window

---
+++ src/MyBudget.UI/Views/ExpenseEditorWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace MyBudget.UI.Views
+{
+    public partial class ExpenseEditorWindow : Window
+    {
+        public ExpenseEditorWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
