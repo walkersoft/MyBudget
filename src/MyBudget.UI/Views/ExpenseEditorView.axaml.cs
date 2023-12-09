3b97ff03e14ea4fed36ce25955ac3500311f5189
Jason L. Walker <jasonlanewalker@gmail.com>
2023-12-08 22:41:51 -0600

    Created user control for expense editor

---
+++ src/MyBudget.UI/Views/ExpenseEditorView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace MyBudget.UI.Views
+{
+    public partial class ExpenseEditorView : UserControl
+    {
+        public ExpenseEditorView()
+        {
+            InitializeComponent();
+        }
+    }
+}
