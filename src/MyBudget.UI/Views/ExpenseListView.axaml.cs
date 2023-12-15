b19e6633d4020b23cb68f5a3094b61111a4fae9b
Jason L. Walker <jasonlanewalker@gmail.com>
2023-12-14 22:30:21 -0600

    Added skeleton view and view model for the expense list grid

---
+++ src/MyBudget.UI/Views/ExpenseListView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace MyBudget.UI.Views
+{
+    public partial class ExpenseListView : UserControl
+    {
+        public ExpenseListView()
+        {
+            InitializeComponent();
+        }
+    }
+}
