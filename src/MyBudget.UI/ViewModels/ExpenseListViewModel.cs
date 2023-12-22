86eac2faca7288da95636c6bf6dd626973fe1c48
Jason L. Walker <jasonlanewalker@gmail.com>
2023-12-22 01:01:47 -0600

    Able to get expenses into data grid

--- src/MyBudget.UI/ViewModels/ExpenseListViewModel.cs
+++ src/MyBudget.UI/ViewModels/ExpenseListViewModel.cs
@@ -1,42 +1,37 @@
-﻿using MyBudget.Application;
+﻿using Avalonia.Controls;
+using MyBudget.Application;
 using MyBudget.Application.Entities;
 using ReactiveUI;
 using System;
 using System.Collections.ObjectModel;
+using System.Reactive.Concurrency;
 using System.Threading.Tasks;
 
 namespace MyBudget.UI.ViewModels
 {
     public class ExpenseListViewModel : ViewModelBase
     {
-        //private readonly BudgetApplication app = App.GetBudgetApp();
+        private readonly BudgetApplication app;
 
-        public ObservableCollection<Expense> Expenses { get; }
+        public ObservableCollection<Expense> Expenses { get; } = new();
 
         public ExpenseListViewModel()
         {
-            Expenses = new ObservableCollection<Expense>
+            if (!Design.IsDesignMode)
             {
-                new Expense()
-                {
-                    Source = "Verizon",
-                    ExpenseType = ExpenseType.Stable,
-                    Amount = 200m,
-                    EffectiveDate = DateOnly.FromDateTime(DateTime.Now),
-                }
-            };
-
-            LoadExpensesAsync();
+                app = App.GetBudgetApp();
+                RxApp.MainThreadScheduler.Schedule(LoadExpensesAsync);
+            }
         }
 
-        private async Task LoadExpensesAsync()
+        private async void LoadExpensesAsync()
         {
-            //var expenses = new ObservableCollection<Expense>(await app.GetAllExpensesAsync());
-            //Expenses.Clear();
-            //foreach (var expense in expenses)
-            //{
-            //    Expenses.Add(expense);
-            //}
+            var expenses = new ObservableCollection<Expense>(await app.GetAllExpensesAsync());
+            Expenses.Clear();
+            foreach (var expense in expenses)
+            {
+                Expenses.Add(expense);
+            }
         }
     }
 }
