51c38a84f6d72d93ce17f03028e3d7fb26d63c12
Jason L. Walker <jasonlanewalker@gmail.com>
2023-12-21 21:14:37 -0600

    Created message to raise when expenses change

---
+++ src/MyBudget.UI/Messages/ExpensesChanged.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using MyBudget.Application;
+using MyBudget.UI.ViewModels;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyBudget.UI.Messages
+{
+    internal record ExpensesChanged(ExpenseListViewModel ExpenseListViewModel) : INotification;
+
+    internal class ExpensesChangedHandler(BudgetApplication app) : INotificationHandler<ExpensesChanged>
+    {
+        public async Task Handle(ExpensesChanged notification, CancellationToken cancellationToken)
+        {
+            notification.ExpenseListViewModel.Expenses.Clear();
+            foreach (var expense in await app.GetAllExpensesAsync())
+            {
+                notification.ExpenseListViewModel.Expenses.Add(expense);
+            }
+        }
+    }
+}
--- src/MyBudget.UI/ViewModels/ExpenseListViewModel.cs
+++ src/MyBudget.UI/ViewModels/ExpenseListViewModel.cs
@@ -1,13 +1,23 @@
-﻿using System;
+﻿using MyBudget.Application;
+using MyBudget.Application.Common;
+using MyBudget.Application.Entities;
+using ReactiveUI;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MyBudget.UI.ViewModels
 {
-    public class ExpenseListViewModel
+    public class ExpenseListViewModel() : ViewModelBase
     {
-
+        private ObservableCollection<Expense> expenses = new();
+        public ObservableCollection<Expense> Expenses
+        {
+            get => expenses;
+            set => this.RaiseAndSetIfChanged(ref expenses, value);
+        }
     }
 }
