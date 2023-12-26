eb8f15202d985d67e84fb6d2e71312314e3f648c
Jason L. Walker <jasonlanewalker@gmail.com>
2023-12-26 01:02:07 -0600

    Hooked up data context to category view model; fixed namespace

--- src/MyBudget.UI/ViewModels/CategoryEditorViewModel.cs
+++ src/MyBudget.UI/ViewModels/CategoryEditorViewModel.cs
@@ -1,4 +1,5 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using Avalonia.Controls;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using MyBudget.Application;
 using System.Threading.Tasks;
@@ -7,10 +8,18 @@ namespace MyBudget.UI.ViewModels
 {
     public partial class CategoryEditorViewModel : ViewModelBase
     {
-        private readonly BudgetApplication app = App.GetBudgetApp();
+        private readonly BudgetApplication app;
 
         [ObservableProperty]
         private string categoryName = string.Empty;
+
+        public CategoryEditorViewModel()
+        {
+            if (!Design.IsDesignMode)
+            {
+                app = App.GetBudgetApp();
+            }
+        }
 
         [RelayCommand]
         private async Task SaveCategory()
