e51604bd4f98cfc842e6fc00b98dae4266ce9b8d
Jason L. Walker <jasonlanewalker@gmail.com>
2023-12-26 13:54:09 -0600

    Removed unused code and usings

--- src/MyBudget.UI/ViewModels/MainWindowViewModel.cs
+++ src/MyBudget.UI/ViewModels/MainWindowViewModel.cs
@@ -1,23 +1,17 @@
 ﻿using Avalonia.Controls;
-using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using MyBudget.UI.Views;
-using ReactiveUI;
-using System.Reactive;
-using System.Reactive.Linq;
 using System.Threading.Tasks;
 
 namespace MyBudget.UI.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    //public Interaction<ExpenseEditorViewModel, Unit> ShowExpenseEditor { get; }
     private readonly Window parentWindow;
     
     public MainWindowViewModel(Window parent)
     {
         parentWindow = parent;
-        //ShowExpenseEditor = new Interaction<ExpenseEditorViewModel, Unit>();
     }
 
     [RelayCommand]
--- src/MyBudget.UI/Views/MainWindow.axaml.cs
+++ src/MyBudget.UI/Views/MainWindow.axaml.cs
@@ -1,10 +1,4 @@
 using Avalonia.Controls;
-using Avalonia.ReactiveUI;
-using CommunityToolkit.Mvvm.Input;
-using MyBudget.UI.ViewModels;
-using ReactiveUI;
-using System.Reactive;
-using System.Threading.Tasks;
 
 namespace MyBudget.UI.Views;
 
@@ -13,18 +7,5 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-        //this.WhenActivated(action => action(ViewModel!.ShowExpenseEditor.RegisterHandler(ShowExpenseEditorAsync)));
     }
-
-    
-    //private async Task ShowExpenseEditor(InteractionContext<ExpenseEditorViewModel, Unit> interactionContext)
-    //{
-    //    var dialog = new ExpenseEditorWindow
-    //    {
-    //        DataContext = interactionContext.Input
-    //    };
-
-    //    await dialog.ShowDialog(this);
-    //    interactionContext.SetOutput(Unit.Default);
-    //}
 }
