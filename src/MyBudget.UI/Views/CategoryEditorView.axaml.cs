2b4ff9aa3539563db6a8181da8c3d8e81445d9e9
Jason L. Walker <jasonlanewalker@gmail.com>
2023-12-25 23:02:17 -0600

    Created initial category editor view and empty view model

---
+++ src/MyBudget.UI/Views/CategoryEditorView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace MyBudget.UI;
+
+public partial class CategoryEditorView : UserControl
+{
+    public CategoryEditorView()
+    {
+        InitializeComponent();
+    }
+}
