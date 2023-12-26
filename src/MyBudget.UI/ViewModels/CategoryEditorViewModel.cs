e2150fd9bc97f72cac0e9ded8f01e6bd4c9d6091
Jason L. Walker <jasonlanewalker@gmail.com>
2023-12-26 00:47:33 -0600

    Added command for saving category

--- src/MyBudget.UI/ViewModels/CategoryEditorViewModel.cs
+++ src/MyBudget.UI/ViewModels/CategoryEditorViewModel.cs
@@ -1,19 +1,21 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using MyBudget.Application;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace MyBudget.UI.ViewModels
 {
-    public partial class CategoryEditorViewModel : ObservableObject
+    public partial class CategoryEditorViewModel : ViewModelBase
     {
         private readonly BudgetApplication app = App.GetBudgetApp();
 
         [ObservableProperty]
         private string categoryName = string.Empty;
+
+        [RelayCommand]
+        private async Task SaveCategory()
+        {
+            await app.CreateCategoryAsync(CategoryName);
+        }
     }
 }
