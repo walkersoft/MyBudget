ccf5959a51e5d132ada8f15a3dedfb1bf69a087b
Jason L. Walker <jasonlanewalker@gmail.com>
2023-12-31 17:04:27 -0600

    Quasi-useful rendition of data error notification in category view model

--- src/MyBudget.UI/ViewModels/CategoryEditorViewModel.cs
+++ src/MyBudget.UI/ViewModels/CategoryEditorViewModel.cs
@@ -4,15 +4,20 @@ using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
 using MyBudget.Application;
 using MyBudget.UI.Messages;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Threading.Tasks;
 
 namespace MyBudget.UI.ViewModels
 {
-    public partial class CategoryEditorViewModel : ViewModelBase
+    public partial class CategoryEditorViewModel : ViewModelBase, INotifyDataErrorInfo
     {
         private readonly BudgetApplication app;
 
         [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(SaveCategoryCommand))]
         private string categoryName = string.Empty;
 
         public CategoryEditorViewModel()
@@ -23,11 +28,69 @@ namespace MyBudget.UI.ViewModels
             }
         }
 
-        [RelayCommand]
+        private bool anAttemptWasMade = false;
+        private readonly Dictionary<string, string> errors = [];
+
+        public bool HasErrors => anAttemptWasMade && errors.Count > 0;
+        public event EventHandler<DataErrorsChangedEventArgs>? ErrorsChanged;
+
+        public IEnumerable GetErrors(string? propertyName)
+        {
+            return anAttemptWasMade
+                ? errors.GetValueOrDefault(propertyName, null)
+                : null;
+        }
+
+        private void AddError(string propertyName, string message)
+        {
+            errors.TryAdd(propertyName, message);
+            OnErrorsChanged(this, new DataErrorsChangedEventArgs(propertyName));
+        }
+        
+        private void RemoveError(string propertyName)
+        {
+            errors.Remove(propertyName);
+            OnErrorsChanged(this, new DataErrorsChangedEventArgs(propertyName));
+        }
+
+        private void OnErrorsChanged(object sender, DataErrorsChangedEventArgs e)
+        {
+            ErrorsChanged?.Invoke(this, e);
+        }
+
+        private void ClearErrors()
+        {
+            errors.Clear();
+        }
+
+        [RelayCommand(CanExecute = nameof(CanSaveCategory))]
         private async Task SaveCategory()
         {
-            await app.CreateCategoryAsync(CategoryName);
-            Messenger.Send(new CategoriesChanged());
+            anAttemptWasMade = true;
+            if (CanSaveCategory())
+            {
+                await app.CreateCategoryAsync(CategoryName);
+                Messenger.Send(new CategoriesChanged());
+            }
+        }
+
+        private bool CanSaveCategory()
+        {
+            // Validate
+            if (anAttemptWasMade)
+            {
+                ClearErrors();
+                if (CategoryName.Length == 0)
+                {
+                    AddError(nameof(CategoryName), "Category name cannot be empty.");
+                }
+                else
+                {
+                    RemoveError(nameof(CategoryName));
+                }
+            }
+
+            return !HasErrors;
         }
     }
 }
