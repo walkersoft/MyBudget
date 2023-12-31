992da88b009781dd97e1df803d7bcfa3d82f2107
Jason L. Walker <jasonlanewalker@gmail.com>
2023-12-30 20:33:37 -0600

    Updated service configuration for validators that aren't public

--- src/MyBudget.Application/ConfigureServices.cs
+++ src/MyBudget.Application/ConfigureServices.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
 using MyBudget.Application.Pipeline;
 using System.Reflection;
 
@@ -8,12 +9,17 @@ namespace MyBudget.Application
     {
         public static void ConfigureBudgetApplication(this IServiceCollection services)
         {
+            services.AddSingleton<BudgetApplication>();
+            
             services.AddMediatR(config => {
                 config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
                 config.AddOpenBehavior(typeof(ValidationBehavior<,>));
             });
 
-            services.AddSingleton<BudgetApplication>();
+            // Manually scan the assembly for validators since internal class won't be
+            // picked up by FluentValidations
+            var types = Assembly.GetExecutingAssembly().GetTypes();
+            new AssemblyScanner(types).ForEach(pair => services.AddTransient(pair.InterfaceType, pair.ValidatorType));
         }
     }
 }
