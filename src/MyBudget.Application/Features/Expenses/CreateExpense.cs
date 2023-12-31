53fd4e37dca0e5292ec82c4b4d4c99e20defbb8c
Jason L. Walker <jasonlanewalker@gmail.com>
2023-12-30 19:40:38 -0600

    Created validator for expense creation

--- src/MyBudget.Application/Features/Expenses/CreateExpense.cs
+++ src/MyBudget.Application/Features/Expenses/CreateExpense.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+﻿using FluentValidation;
+using MediatR;
 using MyBudget.Application.Common;
 using MyBudget.Application.Entities;
 
@@ -12,6 +13,27 @@ namespace MyBudget.Application.Features.Expenses
         decimal? Amount = null,
         Guid? ExpenseCategoryId = null
     ) : IRequest<Expense>;
+
+    internal class CreateExpenseValidator : AbstractValidator<CreateExpense>
+    {
+        public CreateExpenseValidator()
+        {
+            // Rules for all expenses
+            RuleFor(x => x.ExpenseType).IsInEnum().WithMessage("An unknown expense type was provided.");
+            RuleFor(x => x.Source).NotEmpty().WithMessage("Expense source cannot be empty.");
+            RuleFor(x => x.EffectiveDate).NotEmpty().WithMessage("Effective date is required.");
+
+            // Conditional rules
+            RuleFor(x => x.ExpirationDate)
+                .NotNull().When(x => x.ExpenseType == ExpenseType.Fixed).WithMessage("Expiration date is required for fixed expenses.")
+                .GreaterThan(x => x.EffectiveDate).When(x => x.ExpirationDate.HasValue).WithMessage("Expiration date must be after effective date");
+
+            RuleFor(x => x.Amount)
+                .Cascade(CascadeMode.Stop)
+                .NotNull().When(x => x.ExpenseType != ExpenseType.Variable).WithMessage("Amount is required for this expense type.")
+                .GreaterThan(0).WithMessage("Amount must be greater than zero.");
+        }
+    }
 
     internal class CreateExpenseHandler(IAppDataContext context) : IRequestHandler<CreateExpense, Expense>
     {
