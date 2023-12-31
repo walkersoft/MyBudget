ef3c014c5bb38a378b56084a71fbbae614d67e97
Jason L. Walker <jasonlanewalker@gmail.com>
2023-12-30 20:40:35 -0600

    Added validation tests for non-conditional checks

--- tests/MyBudget.Application.Tests/Features/Expenses/CreateExpenseTests.cs
+++ tests/MyBudget.Application.Tests/Features/Expenses/CreateExpenseTests.cs
@@ -26,5 +26,29 @@ namespace MyBudget.Application.Tests.Features.Expenses
 
             await action.Should().ThrowAsync<ValidationException>();
         }
+
+        [Fact]
+        public async Task ExpenseWithoutSource_WhenCreated_ThrowsException()
+        {
+            var action = async () => await app.CreateExpenseAsync(
+                ExpenseType.Variable,
+                string.Empty,
+                DateOnly.FromDateTime(DateTime.Today)
+            );
+
+            await action.Should().ThrowAsync<ValidationException>();
+        }
+
+        [Fact]
+        public async Task ExpenseWithoutEffectiveDate_WhenCreated_ThrowsException()
+        {
+            var action = async () => await app.CreateExpenseAsync(
+                ExpenseType.Variable,
+                "Source",
+                default
+            );
+
+            await action.Should().ThrowAsync<ValidationException>();
+        }
     }
 }
