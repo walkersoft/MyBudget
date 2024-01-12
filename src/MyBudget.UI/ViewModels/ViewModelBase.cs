d19d3a4bb5e9e57383c1c106be2e782e254becb3
Jason L. Walker <jasonlanewalker@gmail.com>
2024-01-11 18:36:34 -0600

    Override default behavior of validator's HasErrors

--- src/MyBudget.UI/ViewModels/ViewModelBase.cs
+++ src/MyBudget.UI/ViewModels/ViewModelBase.cs
@@ -6,6 +6,7 @@ namespace MyBudget.UI.ViewModels;
 public class ViewModelBase : ObservableValidator
 {
     protected static WeakReferenceMessenger Messenger => WeakReferenceMessenger.Default;
+    public new bool HasErrors => ValidationEnabled && base.HasErrors;
     public bool ValidationEnabled { get; set; }
     protected bool CanExecute => !ValidationEnabled || (ValidationEnabled && !HasErrors);
     
