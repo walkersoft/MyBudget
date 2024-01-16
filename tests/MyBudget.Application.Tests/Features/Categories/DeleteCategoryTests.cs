3516b8b357fbdbc2dfa5135d005bcce279a07d21
Jason L. Walker <jasonlanewalker@gmail.com>
2024-01-16 17:57:08 -0600

    Added test to confirm exception thrown when deleting a category that has been assigned

--- tests/MyBudget.Application.Tests/Features/Categories/DeleteCategoryTests.cs
+++ tests/MyBudget.Application.Tests/Features/Categories/DeleteCategoryTests.cs
@@ -5,14 +5,25 @@ namespace MyBudget.Application.Tests.Features.Categories
     public sealed class DeleteCategoryTests : TestBench
     {
         [Fact]
-        public async Task GivenExistingCategory_WhenUnassigned_CanBeDeleted()
+        public async Task GivenExistingCategory_WhenDeleted_WillSucceed()
         {
-            var expense = await CreateCategoryAsync();
+            var category = await CreateCategoryAsync();
 
-            await app.DeleteCategoryAsync(expense.Id);
+            await app.DeleteCategoryAsync(category.Id);
             var categories = await app.GetAllCategoriesAsync();
 
             categories.Should().BeEmpty();
+        }
+
+        [Fact]
+        public async Task GivenExistingCategoryAssignedToExpense_WhenDeleted_WillThrowException()
+        {
+            var category = await CreateCategoryAsync();
+            await CreateVariableExpenseAsync(category.Id);
+
+            var action = async () => await app.DeleteCategoryAsync(category.Id);
+
+            await action.Should().ThrowAsync<ArgumentException>();
         }
     }
 }
