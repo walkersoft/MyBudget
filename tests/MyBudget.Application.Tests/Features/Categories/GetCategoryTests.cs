ca72fb9927e76dc1b34ed53202aa3bfc8ce867aa
Jason L. Walker <jasonlanewalker@gmail.com>
2024-02-28 21:30:32 -0600

    Created failing test to fetch a category

---
+++ tests/MyBudget.Application.Tests/Features/Categories/GetCategoryTests.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+
+namespace MyBudget.Application.Tests.Features.Categories
+{
+    public sealed class GetCategoryTests : TestBench
+    {
+        [Fact]
+        public async Task GivenCategoryExists_WhenFetched_WillSucceed()
+        {
+            var category = await CreateCategoryAsync();
+
+            var action = async () => await app.GetCategoryAsync(category.Id);
+
+            await action.Should().NotThrowAsync();
+        }
+    }
+}
