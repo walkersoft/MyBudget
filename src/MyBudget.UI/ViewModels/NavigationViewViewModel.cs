ca129f83b173bc5588e7b59656b16e3dd4262c5b
Jason L. Walker <jasonlanewalker@gmail.com>
2024-03-24 18:10:18 -0500

    Add view model for nav and setup commands to issue view swap messages

---
+++ src/MyBudget.UI/ViewModels/NavigationViewViewModel.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using MyBudget.UI.Messages;
+
+namespace MyBudget.UI.ViewModels
+{
+    public partial class NavigationViewViewModel : ViewModelBase
+    {
+        public NavigationViewViewModel()
+        {
+            Messenger.RegisterAll(this);
+        }
+
+        [RelayCommand]
+        public void LoadHomeView() => Messenger.Send(new LoadHomeView());
+
+        [RelayCommand]
+        public void LoadExpenseEditorView() => Messenger.Send(new LoadExpenseEditorView());
+    }
+}
